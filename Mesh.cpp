63f473cc0934ef75412b4dd686805cf2583a4ee0
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2023-12-28 10:41:34 +0100

    Mesh...

---
+++ Mesh.cpp
@@ -0,0 +1,9 @@
+#include "Mesh.h"
+
+class Mesh
+{
+	Mesh(Vertex[] _vertices, uint16_t[] _indices, ComPtr<ID3D12Device> md3dDevice, ComPtr<ID3D12GraphicsCommandList> mCommandList)
+	{
+		
+	}
+};
--- Mesh.h
+++ Mesh.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include "Vertex.h"
+#include "Common/d3dApp.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace std;
 
 class Mesh
 {
 public:
+	Mesh(Vertex[] _vertices, uint16_t[] _indices, ComPtr<ID3D12Device> md3dDevice, ComPtr<ID3D12GraphicsCommandList> mCommandList);
 
 private:
-	//Vertex[] vertices;
+	Vertex[] vertices;
+	uint16_t[] indices;
+
+	unique_ptr<MeshGeometry> mGeometry = nullptr;
 };
 
