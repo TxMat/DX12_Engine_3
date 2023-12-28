36cb3f7ae84bab219ce72b4fbb96233776d9881b
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2023-12-28 10:59:46 +0100

    Update Mesh.cpp

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,10 +1,7 @@
 #include "Mesh.h"
 
-class Mesh
+Mesh::Mesh(vector<Vertex> _vertices, vector<uint16_t> _indices, ComPtr<ID3D12Device> md3dDevice, ComPtr<ID3D12GraphicsCommandList> mCommandList)
 {
-	Mesh(vector<Vertex> _vertices, vector<uint16_t> _indices, ComPtr<ID3D12Device> md3dDevice, ComPtr<ID3D12GraphicsCommandList> mCommandList)
-	{
-		Mesh::vertices = _vertices;
-		Mesh::indices = _indices;
-	}
-};
+	Mesh::vertices = _vertices;
+	Mesh::indices = _indices;
+}
