e9916897fa5711ea6342bc310c260e94613dfce7
Tx Mat <46858765+TxMat@users.noreply.github.com>
2023-12-28 11:59:45 +0100

    Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -1,1 +1,6 @@
 ﻿#include "Object.h"
+
+Object::Object()
+{
+    
+}
--- Object.h
+++ Object.h
@@ -1,8 +1,16 @@
 ﻿#pragma once
+#include "Mesh.h"
 #include "Tranform.h"
 
 class Object
 {
 public:
+    explicit Object(Mesh& m_mesh)
+        : mMesh(m_mesh)
+    {
+        mTransform = Transform();
+    }
+
     Transform mTransform;
+    Mesh& mMesh;
 };
