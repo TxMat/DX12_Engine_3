46ab959bf0712e2da80c32e17afd81e9c3b86649
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2023-12-28 09:26:27 +0100

    Update BoxApp.cpp

--- BoxApp.cpp
+++ BoxApp.cpp
@@ -253,7 +253,7 @@ void BoxApp::OnResize()
 
 void BoxApp::Update(const GameTimer& gt)
 {
-    mTransform.Rotate(0, 0.01f, 0);
+    mTransform.Rotate(0, gt.DeltaTime(), 0);
     //mTransform.TranslateLocal(0, 0, 0.01f);
     //mTransform.Rotate(0, 0, 0);
     mTransform.ApplyChanges();
