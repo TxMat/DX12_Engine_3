92c933b87da5eaa52c3a797c66a263a40c009a9d
Tx Mat <46858765+TxMat@users.noreply.github.com>
2023-12-28 11:03:46 +0100

    proper vector creation

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,7 +1,7 @@
 #include "Mesh.h"
 
-Mesh::Mesh(vector<Vertex>& _vertices, vector<uint16_t>& _indices, ComPtr<ID3D12Device>& md3dDevice, ComPtr<ID3D12GraphicsCommandList>& mCommandList)
+Mesh::Mesh(vector<Vertex>& _vertices, vector<uint16_t>& _indices, ComPtr<ID3D12Device> md3dDevice, ComPtr<ID3D12GraphicsCommandList> mCommandList)
 {
-	vertices = _vertices;
-	indices = _indices;
+	vertices.assign(_vertices.begin(), _vertices.end());
+	indices.assign(_indices.begin(), _indices.end());
 }
--- Mesh.h
+++ Mesh.h
@@ -10,8 +10,8 @@ using namespace std;
 class Mesh
 {
 public:
-	Mesh(vector<Vertex>& _vertices, vector<uint16_t>& _indices, ComPtr<ID3D12Device>& md3dDevice,
-	     ComPtr<ID3D12GraphicsCommandList>& mCommandList);
+	Mesh(vector<Vertex>& _vertices, vector<uint16_t>& _indices, ComPtr<ID3D12Device> md3dDevice,
+	     ComPtr<ID3D12GraphicsCommandList> mCommandList);
 
 private:
 	vector<Vertex> vertices;
